2c23c638835f9e3c31b0ea029759fd7c85ded06e
Larry Hatcher <larry.hatcher@hstpathways.com>
2022-02-17 15:46:29 -0600

    Inital push for front end.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App"
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter} from 'react-router-dom';
+
+ReactDom.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+, document.getElementById('root'));
