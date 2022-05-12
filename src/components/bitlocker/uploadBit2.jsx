73e774597bbf6e38f203ad887b85744a3ce41fda
Larry Hatcher <larry.hatcher@hstpathways.com>
2022-05-12 16:16:01 -0500

    Able to read file correctly. Need to work on xlsx for some reason give an error but xls works. Fixed where array wasn't picking up array correctly.

--- src/components/bitlocker/uploadBit2.jsx
+++ src/components/bitlocker/uploadBit2.jsx
@@ -37,7 +37,8 @@ function NewUpload() {
             const worksheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[worksheetName];
             const data = XLSX.utils.sheet_to_json(worksheet);
-            setExcelData(data);    
+            setExcelData(data); 
+            console.log(excelData)   
         }
         else{
             setExcelData(null);
